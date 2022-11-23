0bfd261e4f9023c9610dd04ebd5c27d1198cea7b
ksteinhok <73804015+ksteinhok@users.noreply.github.com>
2022-11-22 23:33:18 -0800

    slighty newer version of profile page

--- src/Profile.js
+++ src/Profile.js
@@ -10,6 +10,7 @@ import Divider from '@mui/material/Divider';
 import Grid from '@mui/material/Grid';
 import App from './App.js'
 import Background from './music.jpeg'
+import API from './API.png'
 
 
 const ProfilePage = () => {
@@ -18,11 +19,17 @@ const ProfilePage = () => {
 
             <Box sx={{height:250}}className="Header" style={{margin:'auto',backgroundImage:`url(${Background})`, backgroundPosition: "center",backgroundSize: "cover", alignContent:"center"}}>
                 <h2 style={{textAlign:"center",padding:80, margin:'auto', color:'forestgreen', fontSize:'2.25rem'}}>Welcome admin, <br/>try selecting a playlist, or importing a new one from Spotify!</h2></Box>
+        <h1 style={{textAlign:"center"}}>How it works.</h1>
+            <Grid container columns={2} >
 
-            <div><h1>How it works.</h1>
-                <Box>
-                    <p> Utilizing Spotify's various API calls, we are capable of creating sleek and innovative infographics for our users</p>
-                </Box>
+                <Grid item xs={1}>
+
+                    <Box>
+                        <p style={{fontSize:'2rem', justifyContent:"center", textAlign:"center"}}> Utilizing Spotify's various API calls, we are capable of retrieving and storing metadata from songs, playlists, and artists</p>
+                    </Box></Grid>
+                <Grid item xs={1} style={{justifyContent:"center"}}> <img src={API} width={600} style={{padding:20}}/> </Grid>
+            </Grid>
+            <div>
 
 
         </div>
