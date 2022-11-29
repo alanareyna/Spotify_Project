6a3f6be68de2a7311297e6cb58cbf3ce2aa5ee25
Joseph Henderson <hendersonj@sonoma.edu>
2022-11-29 12:17:06 -0800

    Small correction to slim table
    Corrected primary key of genre to reference both the genre and song

--- spotify_api/app/Schema/spotify_tables_slim.sql
+++ spotify_api/app/Schema/spotify_tables_slim.sql
@@ -47,5 +47,6 @@ CREATE TABLE Playlist_Song (
 CREATE TABLE Genres (
 	song VARCHAR(200) PRIMARY KEY,
     genre VARCHAR(50),
+    PRIMARY KEY (song, genre),
     FOREIGN KEY (song) REFERENCES Song(id)
 );
